1c45d8e57cbf072da9497597afa6c9f8de0b79e3
Vitoriono <korovin.vic@gmail.com>
2022-09-19 19:02:43 +0300

    new commit

---
+++ nature/script.js
@@ -0,0 +1,44 @@
+
+
+document.querySelector(".block_a").onmouseenter = () => {
+    document.querySelector(".block_a").style.background = 'rgb(41, 43, 52)';
+    document.querySelector(".block_a").style.width = 450 + 'px';
+    
+}
+
+document.querySelector(".block_a").onmouseleave = () => {
+    document.querySelector(".block_a").style.background = 'rgb(66, 61, 61)';
+    document.querySelector(".block_a").style.width = 400 + 'px';
+   
+}
+
+document.querySelector(".block_b").onmouseenter = () => {
+    document.querySelector(".block_b").style.background = 'rgb(41, 43, 52)';
+    document.querySelector(".block_b").style.width = 450 + 'px';
+    
+}
+
+document.querySelector(".block_b").onmouseleave = () => {
+    document.querySelector(".block_b").style.background = 'rgb(66, 61, 61)';
+    document.querySelector(".block_b").style.width = 400 + 'px';
+    
+}
+
+document.querySelector(".block_c").onmouseenter = () => {
+    document.querySelector(".block_c").style.background = 'rgb(41, 43, 52)';
+    document.querySelector(".block_c").style.width = 450 + 'px';
+    
+}
+
+document.querySelector(".block_c").onmouseleave = () => {
+    document.querySelector(".block_c").style.background = 'rgb(66, 61, 61)';
+    document.querySelector(".block_c").style.width = 400 + 'px';
+    
+}
+
+
+
+
+
+
+
