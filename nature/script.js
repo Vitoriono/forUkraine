7dc4925fdff97c049c405ea8c641ec92f0fcfe08
Vitoriono <korovin.vic@gmail.com>
2022-09-20 13:47:09 +0300

    nova comita

--- nature/script.js
+++ nature/script.js
@@ -1,40 +1,42 @@
 
+let one = document.querySelector(".block_a");
+let two = document.querySelector(".block_b");
+let three = document.querySelector(".block_c");
 
-document.querySelector(".block_a").onmouseenter = () => {
-    document.querySelector(".block_a").style.background = 'rgb(41, 43, 52)';
-    document.querySelector(".block_a").style.width = 450 + 'px';
+    one.onmouseenter = () => {
+    one.style.background = 'rgb(41, 43, 52)';
+    one.style.width = 450 + 'px';
+}
+
+    one.onmouseleave = () => {
+    one.style.background = 'rgb(66, 61, 61)';
+    one.style.width = 400 + 'px';
+}
+
+two.onmouseenter = () => {
+two.style.background = 'rgb(41, 43, 52)';
+    two.style.width = 450 + 'px';
     
 }
 
-document.querySelector(".block_a").onmouseleave = () => {
-    document.querySelector(".block_a").style.background = 'rgb(66, 61, 61)';
-    document.querySelector(".block_a").style.width = 400 + 'px';
-   
-}
-
-document.querySelector(".block_b").onmouseenter = () => {
-    document.querySelector(".block_b").style.background = 'rgb(41, 43, 52)';
-    document.querySelector(".block_b").style.width = 450 + 'px';
+two.onmouseleave = () => {
+    two.style.background = 'rgb(66, 61, 61)';
+    two.style.width = 400 + 'px';
     
 }
 
-document.querySelector(".block_b").onmouseleave = () => {
-    document.querySelector(".block_b").style.background = 'rgb(66, 61, 61)';
-    document.querySelector(".block_b").style.width = 400 + 'px';
+three.onmouseenter = () => {
+    three.style.background = 'rgb(41, 43, 52)';
+    three.style.width = 450 + 'px';
+    
+}
+three.onmouseleave = () => {
+    three.style.background = 'rgb(66, 61, 61)';
+    three.style.width = 400 + 'px';
     
 }
 
-document.querySelector(".block_c").onmouseenter = () => {
-    document.querySelector(".block_c").style.background = 'rgb(41, 43, 52)';
-    document.querySelector(".block_c").style.width = 450 + 'px';
-    
-}
 
-document.querySelector(".block_c").onmouseleave = () => {
-    document.querySelector(".block_c").style.background = 'rgb(66, 61, 61)';
-    document.querySelector(".block_c").style.width = 400 + 'px';
-    
-}
 
 
 
