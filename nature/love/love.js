ddc9792e0a2c9d49e6f7df08a73a88a24a86009b
Vitoriono <korovin.vic@gmail.com>
2022-09-23 22:04:13 +0300

    tonight com

--- nature/love/love.js
+++ nature/love/love.js
@@ -1,7 +1,13 @@
-let btn = document.querySelector('.btn');
+let fullHeight, innerHeight;
+let progressBar = document.querySelector('.progressbar>.grogressbar-line');
 
- btn.onclick = ()=>{
-  document.querySelector('.href') = true;
+window.addEventListener('scroll', fillProgressLine);
+window.addEventListener('resize', fillProgressLine);
+
+function fillProgressLine(){
+  fullHeight = document.body.scrollHeight;
+  innerHeight = window.innerHeight;
+  progressBar.style.width = (scrollY*100 / (fullHeight - innerHeight  )) + '%';
 }
 
-console.log(btn.onclick)
+fillProgressLine();
