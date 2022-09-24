79be03e802c6d41a27973de1733c04511724d8d7
Vitoriono <korovin.vic@gmail.com>
2022-09-24 15:45:41 +0300

    granit clifs

---
+++ nature/granite/granite.js
@@ -0,0 +1,15 @@
+
+let up_nuv = document.querySelector('.topnav');
+
+
+
+up_nuv.onmouseenter = () => {
+  up_nuv.style.opacity = '0.7';
+  up_nuv.style.transition ='all 1s ease';
+}
+
+
+up_nuv.onmouseleave = () => {
+  up_nuv.style.opacity = '0.0';
+}
+
