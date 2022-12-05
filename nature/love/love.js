d28179d1da8de201fdfcaaaaf15131f4abd6f9a2
Vitoriono <korovin.vic@gmail.com>
2022-12-05 12:43:35 +0200

    refactr commit

--- nature/love/love.js
+++ nature/love/love.js
@@ -4,7 +4,7 @@ let progressBar = document.querySelector('.progressbar>.grogressbar-line');
 window.addEventListener('scroll', fillProgressLine);
 window.addEventListener('resize', fillProgressLine);
 
-function fillProgressLine(){
+function fillProgressLine() {
   fullHeight = document.body.scrollHeight;
   innerHeight = window.innerHeight;
   progressBar.style.width = (scrollY*100 / (fullHeight - innerHeight  )) + '%';
